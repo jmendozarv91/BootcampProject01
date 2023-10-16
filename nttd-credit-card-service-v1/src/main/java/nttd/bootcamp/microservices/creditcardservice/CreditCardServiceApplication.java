d948773daa2e3dd0c16af502456da0b9c23c798c
jose mendoza sanchez <jmendozarv@outlook.com>
2023-10-16 14:22:57 -0500

    initial commit

---
+++ nttd-credit-card-service-v1/src/main/java/nttd/bootcamp/microservices/creditcardservice/CreditCardServiceApplication.java
@@ -0,0 +1,13 @@
+package nttd.bootcamp.microservices.creditcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardServiceApplication.class, args);
+	}
+
+}
---
+++ nttd-credit-card-service-v1/src/test/java/nttd/bootcamp/microservices/creditcardservice/CreditCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nttd.bootcamp.microservices.creditcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
