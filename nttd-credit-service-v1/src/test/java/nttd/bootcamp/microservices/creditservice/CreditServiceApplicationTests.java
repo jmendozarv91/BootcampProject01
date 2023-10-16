0288cd64c71c94c4273ba22e9ebd6d0137c5df98
jose mendoza sanchez <jmendozarv@outlook.com>
2023-10-16 09:30:04 -0500

    initial commmit

---
+++ nttd-credit-service-v1/src/test/java/nttd/bootcamp/microservices/creditservice/CreditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nttd.bootcamp.microservices.creditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
