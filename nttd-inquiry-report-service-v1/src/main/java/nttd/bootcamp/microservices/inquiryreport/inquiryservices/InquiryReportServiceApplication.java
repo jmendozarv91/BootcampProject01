3b8adcc8f369a47a1ec23328b067521c75440737
jose mendoza sanchez <jmendozarv@outlook.com>
2023-10-23 14:12:20 -0500

    add new microservice for inquiry report services

---
+++ nttd-inquiry-report-service-v1/src/main/java/nttd/bootcamp/microservices/inquiryreport/inquiryservices/InquiryReportServiceApplication.java
@@ -0,0 +1,13 @@
+package nttd.bootcamp.microservices.inquiryreport.inquiryservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryReportServiceApplication.class, args);
+	}
+
+}
